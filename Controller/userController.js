dfc6d08eab9b21dfd89b0e3b7d1dd1d975a054de
riad-khan <rhkhan2367@gmail.com>
2022-10-17 21:52:14 +0600

    profile

--- Controller/userController.js
+++ Controller/userController.js
@@ -30,7 +30,7 @@ module.exports.signIn = (req, res) => {
   let rows = []
   const email = req.body.email;
   const password = req.body.password;
-  const findUser = mySqlConnection.query('select email,password from users where email = ? LIMIT 1', [email], (error, row, fields) => {
+  const findUser = mySqlConnection.query('select * from users where email = ?', [email], (error, row, fields) => {
     if (row.length === 0) {
       return res.status(404).send('user not found in database');
     } else {
@@ -47,6 +47,8 @@ module.exports.signIn = (req, res) => {
           return res.send({
             access_token: jwt
           });
+        } else {
+          return res.status(400).send("incorrect password")
         }
       } else {
         return res.status(400).send("please Check your credentials")
@@ -59,11 +61,72 @@ module.exports.signIn = (req, res) => {
 
 
 
+
   })
 }
 
-module.exports.userInfo = (req,res)=>{
+module.exports.userInfo = (req, res) => {
   const id = req.params.id;
   let sql = 'select profile_pic,name from users where id =?';
-  const data = mysql.query()
+  const data = mySqlConnection.query(sql, (id), (error, result, fields) => {
+    if (!error) {
+      return res.send(result);
+    } else {
+      console.log(error);
+    }
+  })
+}
+
+module.exports.updateProfile = async (req, res) => {
+  const name = JSON.stringify(req.body.name);
+  const email = JSON.stringify(req.body.email);
+  const password = req.body.password;
+  const captcha = req.body.captcha === true ? 1 : 0;
+
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  let sql = `update users set name = ${name},password = ${JSON.stringify(hashPassword)},captcha = ${captcha} where email = ${email}`;
+
+  mySqlConnection.query(sql, (error, result, fields) => {
+    if (!error) {
+      const jwt = sign({
+        id: req.body.id,
+        name: req.body.name,
+        email: req.body.email,
+      }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+
+      return res.send({
+        message: "Profile updated successfully",
+        token: jwt,
+      });
+    } else {
+      console.log(error);
+    }
+  })
+
+}
+
+module.exports.fetchUserComments = async (req, res) => {
+  const id = req.params.id;
+  let sql = `select a.*,b.title from news_comments a left join news b on a.news_id = b.id where a.user_id = ${id}`;
+  mySqlConnection.query(sql, (error, results, fields) => {
+      if(!error){
+        return res.status(200).send(results);
+      }else{
+        console.log(error);
+      }
+  })
+}
+module.exports.deleteComments = async (req, res)=>{
+    const id = req.params.id;
+    const user_id = req.params.user_id;
+    let sql = `delete from news_comments where id = ${id} and user_id = ${user_id}`;
+    mySqlConnection.query(sql,(error,results,fields)=>{
+        if(!error){
+          return res.status(200).send('Comments Deleted Successfully')
+        }else{
+          console.log(error);
+        };
+    })
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signUp,signIn } = require('../Controller/userController');
+const { signUp, signIn, userInfo, updateProfile,fetchUserComments,deleteComments } = require('../Controller/userController');
 const authorization = require('../middlewares/authorization');
 
 const router = express.Router();
@@ -8,13 +8,16 @@ router.route('/sign-up')
     .post(signUp);
 
 router.route('/sign-in')
-        .post(signIn)
+    .post(signIn)
 
-        router.route('/user/:id')
-        .get(authorization,userInfo)
+router.route('/user-info/:id')
+    .get(authorization, userInfo);
+
+router.route('/update/profile').post(authorization,updateProfile);
+router.route('/comments/:id').get(authorization,fetchUserComments);
+router.route('/delete-comments/:id/:user_id').delete(deleteComments)
 
 
-        
 
-    
- module.exports = router
+
+module.exports = router
