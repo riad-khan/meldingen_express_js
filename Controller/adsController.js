37f3a34a0aab643cc751ddfbd204434f709f268c
riad-khan <rhkhan2367@gmail.com>
2022-12-05 02:07:16 +0600

    update

--- Controller/adsController.js
+++ Controller/adsController.js
@@ -27,22 +27,25 @@ module.exports.getHomePageAds = async (req, res) => {
 module.exports.meldingenDetailsAds = async (req, res) => {
     const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=4 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
     const sql2 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=5 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
+    const sql3 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=6 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
 
 
     const ad1 = await ads(sql1);
     const ad2 = await ads(sql2);
+    const ad3 = await ads(sql3);
 
 
     return res.send({
         ad1: ad1,
         ad2: ad2,
+        ad3: ad3,
 
     })
 }
 module.exports.newsAds = async (req, res) => {
-    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=6 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
-    const sql2 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=7 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
-    const sql3 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=8 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
+    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=7 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
+    const sql2 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=8 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
+    const sql3 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=9 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
 
     const ad1 = await ads(sql1);
     const ad2 = await ads(sql2);
@@ -56,9 +59,9 @@ module.exports.newsAds = async (req, res) => {
 }
 
 module.exports.newsDetails = async (req, res) => {
-    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=9 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
-    const sql2 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=10 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
-    const sql3 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=11 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
+    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=10 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
+    const sql2 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=11 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
+    const sql3 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=12 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1;';
 
     const ad1 = await ads(sql1);
     const ad2 = await ads(sql2);
@@ -71,10 +74,20 @@ module.exports.newsDetails = async (req, res) => {
     })
 }
 module.exports.partnerBlogs = async (req, res) => {
-    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=9 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
+    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=14 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
 
     const ad1 = await ads(sql1);
 
+
+    return res.send({
+        ad1: ad1,
+
+    })
+}
+
+module.exports.blogs = async (req, res)=>{
+    const sql1 = 'SELECT content FROM `ads` WHERE content<>"" and status=1 and section=13 and DATE_FORMAT(curtime(), "%H:%i")>=from_hr and DATE_FORMAT(curtime(), "%H:%i")<=to_hr limit 1';
+    const ad1 = await ads(sql1);
 
     return res.send({
         ad1: ad1,
